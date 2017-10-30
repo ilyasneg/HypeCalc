f7b11865db3f5404162b9e8e92fd193d4b59fb56
Илья Снегирев <ilyasneg@live.ru>
2017-10-30 11:32:54 +0400

    Первая версия. всё ладом

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -8,9 +8,24 @@ namespace ConsoleApp
 {
     class Program
     {
+        //operation x y
         static void Main(string[] args)
         {
             Console.WriteLine("Hype Calc");
+
+            if (args.Count() == 3)
+            {
+                var operation = args[0];
+                var x = args[1];
+                var y = args[2];
+                double result;
+
+                if (operation == "sum")
+                {
+                    result = double.Parse(x) + double.Parse(y);
+                }
+            }
+
             Console.ReadKey();
         }
     }
